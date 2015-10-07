73a1e161720d6d4313c9a46e80e3941fc0b889a7
Rob Caiger <rob@clocal.co.uk>
2015-10-07 14:11:53 +0100

    Release sh

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-tar -czf ../release/olcs-postcode/$VERSION.tar.gz \
+tar -czf ../release/address-service/$VERSION.tar.gz \
 composer.phar composer.json composer.lock config module public vendor \
 --exclude="config/autoload/local.php" --exclude="config/autoload/local.php.dist"
