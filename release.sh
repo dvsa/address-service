b573cb6e4c202b2cfcd48bc507171969d4c9e00e
Rob Caiger <rob@clocal.co.uk>
2015-10-07 16:20:21 +0100

    Fixed release

--- release.sh
+++ release.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 tar -czf ../release/address-service/$VERSION.tar.gz \
-composer.phar composer.json composer.lock config module public vendor \
+composer.phar composer.json composer.lock init_autoloader.php \
+config module public vendor \
 --exclude="config/autoload/local.php" --exclude="config/autoload/local.php.dist"
